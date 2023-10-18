eb6ae1490e742a4f0c3b72d3ea3221a2fc5b89b2
sachin93094 <111477307+sachin93094@users.noreply.github.com>
2023-10-18 15:48:34 +0530

    Update clone.py

--- clone.py
+++ clone.py
@@ -12,6 +12,15 @@ try:
 except subprocess.CalledProcessError as e:
     print(f"Repository cloning failed: {e}")
 
+# Get the latest tag for the first repository
+repo1 = git.Repo(clone_directory1)
+
+try:
+    latest_tag1 = repo1.git.describe(tags=True, abbrev=0)
+    print(f"Latest tag for '{clone_directory1}': {latest_tag1}")
+except git.exc.GitCommandError as e:
+    print(f"Error getting the latest tag for '{clone_directory1}': {e}")
+
 # Define the SSH URL of the second Git repository to clone
 repository_url2 = "git@github.com:sachin93094/git_remote_demorepo1.git"
 clone_directory2 = "git_remote_demorepo1"
@@ -23,12 +32,11 @@ try:
 except subprocess.CalledProcessError as e:
     print(f"Repository cloning failed: {e}")
 
-# Get the latest tag for the first repository
-repo1 = git.Repo(clone_directory1)
-latest_tag1 = repo1.git.describe(tags=True, abbrev=0)
-print(f"Latest tag for '{clone_directory1}': {latest_tag1}")
-
 # Get the latest tag for the second repository
 repo2 = git.Repo(clone_directory2)
-latest_tag2 = repo2.git.describe(tags=True, abbrev=0)
-print(f"Latest tag for '{clone_directory2}': {latest_tag2}")
+
+try:
+    latest_tag2 = repo2.git.describe(tags=True, abbrev=0)
+    print(f"Latest tag for '{clone_directory2}': {latest_tag2}")
+except git.exc.GitCommandError as e:
+    print(f"Error getting the latest tag for '{clone_directory2}': {e}")
