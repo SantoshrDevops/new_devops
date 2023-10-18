20e6a0bf368f7ca782a71d3c2a4b3ef38a3eb20a
sachin93094 <111477307+sachin93094@users.noreply.github.com>
2023-10-18 15:32:22 +0530

    Update clone.py

--- clone.py
+++ clone.py
@@ -1,14 +1,23 @@
 import subprocess
 
-# Define the SSH URL of the Git repository to clone
-repository_url = "git@github.com:sachin93094/private_Repo.git"
+# Define the SSH URL of the first Git repository to clone
+repository_url1 = "git@github.com:sachin93094/private_Repo.git"
+clone_directory1 = "private_Repo"
 
-# Directory where you want to clone the repository
-clone_directory = "private_Repo"
-
-# Clone the repository
+# Clone the first repository
 try:
-    subprocess.check_call(["git", "clone", repository_url, clone_directory])
-    print("Repository cloned successfully in teamcity.")
+    subprocess.check_call(["git", "clone", repository_url1, clone_directory1])
+    print(f"Repository '{clone_directory1}' cloned successfully.")
+except subprocess.CalledProcessError as e:
+    print(f"Repository cloning failed: {e}")
+
+# Define the SSH URL of the second Git repository to clone
+repository_url2 = "git@github.com:sachin93094/git_remote_demorepo1.git"
+clone_directory2 = "git_remote_demorepo1"
+
+# Clone the second repository
+try:
+    subprocess.check_call(["git", "clone", repository_url2, clone_directory2])
+    print(f"Repository '{clone_directory2}' cloned successfully.")
 except subprocess.CalledProcessError as e:
     print(f"Repository cloning failed: {e}")
