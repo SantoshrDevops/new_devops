1bdb5cafd2c1a1595609fa2c0b6c2c3b6fedd140
sachin93094 <111477307+sachin93094@users.noreply.github.com>
2023-10-18 17:24:12 +0530

    Update clone.py

--- clone.py
+++ clone.py
@@ -12,6 +12,15 @@ try:
 except subprocess.CalledProcessError as e:
     print(f"Repository cloning failed: {e}")
 
+# Get the latest tag for the first repository
+repo1 = git.Repo(clone_directory1)
+
+try:
+    latest_tag1 = repo1.git.describe(tags=True, abbrev=0)
+    print(f"Latest tag for '{clone_directory1}': {latest_tag1}")
+except git.exc.GitCommandError as e:
+    print(f"Error getting the latest tag for '{clone_directory1}': {e}")
+
 # Define the SSH URL of the second Git repository to clone
 repository_url2 = "git@github.com:sachin93094/git_remote_demorepo1.git"
 clone_directory2 = "git_remote_demorepo1"
@@ -23,13 +32,11 @@ try:
 except subprocess.CalledProcessError as e:
     print(f"Repository cloning failed: {e}")
 
-# Define the SSH URL of the third Git repository to clone
-repository_url3 = "git@github.com:sachinsdevops/sample_teamcity.git"
-clone_directory3 = "sample_teamcity"
+# Get the latest tag for the second repository
+repo2 = git.Repo(clone_directory2)
 
-# Clone the third repository
 try:
-    subprocess.check_call(["git", "clone", repository_url3, clone_directory3])
-    print(f"Repository '{clone_directory3}' cloned successfully.")
-except subprocess.CalledProcessError as e:
-    print(f"Repository cloning failed: {e}")
+    latest_tag2 = repo2.git.describe(tags=True, abbrev=0)
+    print(f"Latest tag for '{clone_directory2}': {latest_tag2}")
+except git.exc.GitCommandError as e:
+    print(f"Error getting the latest tag for '{clone_directory2}': {e}")
