41412ef6774c30fee5879498cbe55cb77004b353
sachin93094 <111477307+sachin93094@users.noreply.github.com>
2023-11-04 11:55:02 +0530

    Update clone.py

--- clone.py
+++ clone.py
@@ -11,12 +11,16 @@ subprocess.check_call(["git", "config", "--global", "user.name", git_name])
 subprocess.check_call(["git", "config", "--global", "user.email", git_email])
 
 # Define the HTTP URL of the first Git repository to clone
-# Retrieve username and password from TeamCity environment variables
-username = os.environ.get('TC_USERNAME')  # Replace 'TC_USERNAME' with your actual environment variable name
-password = os.environ.get('TC_PASSWORD')  # Replace 'TC_PASSWORD' with your actual environment variable name
+# Retrieve username and Personal Access Token from TeamCity environment variables
+username = os.environ.get('GITHUB_USERNAME')
+token = os.environ.get('GITHUB_TOKEN')
 
-# Construct the repository URL with credentials
-repository_url1 = f"https://{username}:{password}@github.com/sachin93094/private_Repo.git"
+if not username or not token:
+    print("GitHub username and token environment variables are not set.")
+    exit(1)
+
+# Construct the repository URL with the Personal Access Token
+repository_url1 = f"https://{username}:{token}@github.com/sachin93094/private_Repo.git"
 
 # Define the directory to clone the first repository
 clone_directory1 = "private_Repo"
@@ -36,51 +40,3 @@ try:
     print(f"Latest tag for '{clone_directory1}': {latest_tag1}")
 except git.exc.GitCommandError as e:
     print(f"Error getting the latest tag for '{clone_directory1}': {e}")
-
-# Define the HTTP URL of the second Git repository to clone
-# Retrieve the username and password from TeamCity environment variables
-username = os.environ.get('TC_USERNAME')  # Replace 'TC_USERNAME' with your actual environment variable name
-password = os.environ.get('TC_PASSWORD')  # Replace 'TC_PASSWORD' with your actual environment variable name
-
-# Construct the repository URL with credentials
-repository_url2 = f"https://{username}:{password}@github.com/sachin93094/git_remote_demorepo1.git"
-
-# Define the directory to clone the second repository
-clone_directory2 = "git_remote_demorepo1"
-
-# Clone the second repository
-try:
-    subprocess.check_call(["git", "clone", repository_url2, clone_directory2])
-    print(f"Repository '{clone_directory2}' cloned successfully.")
-except subprocess.CalledProcessError as e:
-    print(f"Repository cloning failed: {e}")
-
-# Get the latest tag for the second repository
-repo2 = git.Repo(clone_directory2)
-
-try:
-    latest_tag2 = repo2.git.describe(tags=True, abbrev=0)
-    print(f"Latest tag for '{clone_directory2}': {latest_tag2}")
-except git.exc.GitCommandError as e:
-    print(f"Error getting the latest tag for '{clone_directory2}': {e}")
-
-# Define the name of the new tag
-new_tag_name = "v3.0.0"
-
-# Create and push the new tag for the first repository
-try:
-    tag1 = repo1.create_tag(new_tag_name, message="Created a new tag")
-    remote1 = repo1.create_remote('origin', repository_url1)  # Add the HTTP URL as the remote
-    remote1.push("--tags")
-    print(f"Created and pushed tag '{new_tag_name}' for '{clone_directory1}'")
-except git.exc.GitCommandError as e:
-    print(f"Error creating/pushing the tag for '{clone_directory1}': {e}")
-
-# Create and push the new tag for the second repository
-try:
-    tag2 = repo2.create_tag(new_tag_name, message="Created a new tag")
-    remote2 = repo2.create_remote('origin', repository_url2)  # Add the HTTP URL as the remote
-    remote2.push("--tags")
-    print(f"Created and pushed tag '{new_tag_name}' for '{clone_directory2}'")
-except git.exc.GitCommandError as e:
-    print(f"Error creating/pushing the tag for '{clone_directory2}': {e}")
